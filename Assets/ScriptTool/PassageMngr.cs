c4cbaba90c327b5e4b62cbbc67bec22e30b027c0
roecc <76390093+roecc@users.noreply.github.com>
2023-09-01 11:18:43 +0200

    added options to history with style

--- Assets/ScriptTool/PassageMngr.cs
+++ Assets/ScriptTool/PassageMngr.cs
@@ -18,7 +18,7 @@ public class PassageMngr : MonoBehaviour
 	private TMP_Text passage;
 	[SerializeField]
 	private Transform optionPanel;
-	private TMP_Text[] options;
+	//private TMP_Text[] options;
 
 	//content
 	[TextArea(2, 7)]
@@ -30,12 +30,20 @@ public class PassageMngr : MonoBehaviour
     [SerializeField]
 	private optionLink[] optionLinks;
 
+	private TMP_Text history;
+	private string noOptHist;
 
 	public void AddToHistory()
 	{
 		//add passage to history
-		TMP_Text history = GameObject.FindGameObjectWithTag("History").GetComponent<TMP_Text>();
+		history = GameObject.FindGameObjectWithTag("History").GetComponent<TMP_Text>();
 		history.text += passageText + "\n\n";
+
+		noOptHist = history.text;
+
+		//add Options to Hist
+		AddOptionsToHist();
+
 		//scroll to bottom of history
 		StartCoroutine(DelayScrollUpdate(history));
     }
@@ -48,14 +56,14 @@ public class PassageMngr : MonoBehaviour
         history.transform.parent.GetComponent<ScrollRect>().verticalNormalizedPosition = 0f;
     }
 
+	//update objects to reflect inspector settings
 	private void OnValidate()
 	{
 		//update TMP objects
 		title.text = titleText;
 		passage.text = passageText;
 		gameObject.name = titleText;
-
-		//update objects to reflect inspector settings
+		
 		if (optionLinks.Length > optionPanel.childCount)
 			GameObject.Instantiate(optionPrefab, optionPanel);
 		for (int i = 0; i < optionPanel.childCount; i++)
@@ -69,6 +77,16 @@ public class PassageMngr : MonoBehaviour
 				optionPanel.GetChild(i).GetComponent<OptionScr>().nextPassage = optionLinks[i].nextPassage;
 			}
 		}
+	}
+
+	private void AddOptionsToHist()
+	{
+        for (int i = 0; i < optionLinks.Length; i++)
+        {
+			history.text += "<style=link>" + (i+1) + ". " + optionLinks[i].option + "</style>\n";
+        }
+
+		history.text += "\n";
 	}
 }
 
