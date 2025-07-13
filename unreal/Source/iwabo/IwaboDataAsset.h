7540674b83f0670e8a402e22a20385c728237d96
Catherine Claudet <cmclaudet@gmail.com>
2025-07-13 18:52:18 +0200

    Add base for architecture: custom game instance, state & config services, data classes and structs, base DataAsset CPP class

---
+++ unreal/Source/iwabo/IwaboDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "IwaboDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class IWABO_API UIwaboDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
